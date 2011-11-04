c5b53b48cfde60ee6cc648a45cda520ac334cd7f
Federico Vaga <federico.vaga@gmail.com>
2011-11-04 15:54:23 +0100

    drivers/zio-zero: a real driver feeding software-generated data
    This is a working driver, with one cset and three channels that return
    8-bit data. It is the demonstration that the ZIO abstraction is a good
    way to avoid complexity in the low-level drivers.
    
    Signed-off by: Federico Vaga <federico.vaga@gmail.com>
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

---
+++ drivers/zio-zero.c
@@ -0,0 +1,67 @@
+/* Federico Vaga for CERN, 2011, GNU GPLv2 or later */
+/*
+ * zero-zio is a simple zio driver which fill buffer with 0. From the ZIO
+ * point of view is an input device
+ */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/random.h>
+
+#include <linux/zio.h>
+#include <linux/zio-buffer.h>
+
+int zzero_input(struct zio_channel *chan, struct zio_block *block)
+{
+	static uint8_t datum;
+	uint8_t *data;
+	int i;
+
+	switch(chan->index) {
+	case 0: /* zero */
+		memset(block->data, 0x0, block->datalen);
+		break;
+	case 1: /* random */
+		get_random_bytes(block->data, block->datalen);
+		break;
+	case 2: /* sequence */
+		data =(uint8_t *) block->data;
+		for (i = 0; i < block->datalen; i++)
+			data[i] = datum++;
+	}
+	return 0;
+}
+
+static struct zio_device_operations zzero_d_op = {
+	.input_block =		zzero_input,
+};
+static struct zio_cset zzero_cset[] = {
+	{
+		.n_chan =	3,
+		.ssize =	1,
+		.flags =	ZCSET_TYPE_ANALOG | ZCSET_DIR_INPUT,
+	},
+};
+static struct zio_device zzero_dev = {
+	.d_op =			&zzero_d_op,
+	.cset =			zzero_cset,
+	.n_cset =		ARRAY_SIZE(zzero_cset),
+
+};
+
+static int __init zzero_init(void)
+{
+	return zio_register_dev(&zzero_dev, "zzero");
+}
+static void __exit zzero_exit(void)
+{
+	zio_unregister_dev(&zzero_dev);
+}
+
+module_init(zzero_init);
+module_exit(zzero_exit);
+
+MODULE_AUTHOR("Federico Vaga <federico.vaga@gmail.com>");
+MODULE_DESCRIPTION("A zio driver which fakes zero, random and sawtooth input");
+MODULE_LICENSE("GPL");
