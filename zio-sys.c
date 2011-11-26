fbbcc8d600e4ab709f32942135cf8b4651ea7e51
Alessandro Rubini <rubini@gnudd.com>
2011-11-26 12:11:43 +0100

    zio-sys.c: use strict_strtol so we can compile on older kernels
    Even if checkpatch suggests kstrtol, it is not available in 2.6.38 and
    later ones, so I'm sticking with the previous function for now
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>
    Acked-by: Federico Vaga <federico.vaga@gmail.com>

--- zio-sys.c
+++ zio-sys.c
@@ -512,7 +512,7 @@ static ssize_t zattr_store(struct kobject *kobj, struct attribute *attr,
 	spinlock_t *lock;
 
 	pr_debug("%s\n", __func__);
-	err = kstrtol(buf, 0, &val);
+	err = strict_strtol(buf, 0, &val);
 	if (err)
 		return -EINVAL;
 	if (zattr->s_op->conf_set) {
