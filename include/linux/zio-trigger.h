ce77f08c6f3984be0dbdbe1ee6018fb2658ec71e
Alessandro Rubini <rubini@gnudd.com>
2012-02-06 23:03:40 +0100

    zio-trigger.h: comment fix (for 5e3c6c8)
    We don't have file_operations in the trigger since 5e3c6c8,
    remove the related comment too (we forgot earlier).
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>
    Acked-by: Federico Vaga <federico.vaga@gmail.com>

--- include/linux/zio-trigger.h
+++ include/linux/zio-trigger.h
@@ -14,7 +14,6 @@ struct zio_trigger_type {
 	struct spinlock		lock;
 	unsigned long		flags; /* to be defined */
 
-	/* file_operations because the trigger may override the buffer */
 	const struct zio_sysfs_operations	*s_op;
 	const struct zio_trigger_operations	*t_op;
 
