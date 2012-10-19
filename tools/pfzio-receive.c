c2b52ce73d1c3b2be22b748912dfe251d1adfce1
Alessandro Rubini <rubini@gnudd.com>
2012-10-19 10:13:42 +0200

    tools: mostly rewritten pfzio-receive
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>
    Acked-by: Federico Vaga <federico.vaga@gmail.com>

--- tools/pfzio-receive.c
+++ tools/pfzio-receive.c
@@ -1,4 +1,3 @@
-#include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,149 +5,106 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/socket.h>
 #include <linux/zio-user.h>
 #include <linux/zio-sock.h>
 
-#define AF_ZIO			28
-
-#define PF_ZIO_BIND		1
-#define BUFF_SIZE_DGRAM_STREAM	32
-#define BUFF_SIZE_RAW		1056
-
-int dumpstruct(void *ptr, int size)
+void print_help(char *name, FILE *f)
 {
-	int ret = 0, i;
-	unsigned char *p = ptr;
-
-	for (i = 0; i < size; ) {
-		printf("%02x", p[i]);
-		i++;
-		printf(i & 3 ? " " : i & 0xf ? "  " : "\n");
-	}
-	if (i & 0xf)
-		printf("\n");
-	return ret;
+	fprintf(f, "%s: Use: \"%s [<option> ...] <ziodev>\"\n", name, name);
+	fprintf(f, "Available options:\n"
+		"   -r          raw socket\n"
+		"   -d          datagram socket (default == stream)\n"
+		"   -s <n>      channel-set (number or \"all\", default 0) \n"
+		"   -c <n>      channel (number or \"all\" default 0)\n"
+		"   -i <num>    device id (default: 0 default 0)\n"
+		"   -v          verbose to stderr\n");
+	exit(1);
 }
 
-void print_help(void)
-{
-	printf("Not enough arguments!\n");
-	printf("Example usage: pf-zio-receive -t dgram -s 0 -c 2\n\n");
-	printf("Available options:\n");
-	printf("\t-t  : socket type. Takes a parameter among \"dgram\", \"stream\" and \"raw\"\n");
-	printf("\t-s  : channel set. Takes a number as argument.\n");
-	printf("\t-c  : channel. Takes a number as argument\n");
-	printf("\t-d  : device id. Takes a number as arguments, default to 0\n");
-	printf("\t-n  : device name. Takes a string identifyng the zio device\n");
-	printf("\t-t  : bind to multiple channels. Takes a parameter among \"cset\" and \"dev\"\n");
-	printf("\t-v  : verbose, prints some additional info. Use only once.\n");
-}
+static char buffer[1024*1024]; /* 1MB! */
 
 int main(int argc, char **argv)
 {
-	int socktype = 0, cset = 0, chan = 0, dev_id = 0;
-	int c, verbose = 0, flag = 0;
-	socklen_t sziolen;
-	int sockfd, rxed;
-	struct sockaddr_zio szio, szfrom;
-	char *optvalue = NULL, *buffer;
+	int socktype = SOCK_STREAM;
+	int c, verbose = 0;
+	socklen_t slen;
+	int sock, rxed;
+	struct sockaddr_zio zaddr, peer;
 
-	if (argc < 5){
-		print_help();
-		return 0;
-	}
+	memset(&zaddr, 0, sizeof(zaddr));
+	zaddr.sa_family = AF_ZIO;
+	slen = sizeof(zaddr);
 
-	while ((c = getopt(argc, argv, "t:s:c:d:n:b")) != -1){
-		switch (c)
-		{
-		case 't':
-			optvalue = optarg;
-			printf("%s\n", optvalue);
-			if (strcmp(optvalue, "dgram") == 0){
-				socktype = SOCK_DGRAM;
-				buffer = malloc(BUFF_SIZE_DGRAM_STREAM);
-			}
-			if (strcmp(optvalue, "stream") == 0){
-				socktype = SOCK_STREAM;
-				buffer = malloc(BUFF_SIZE_DGRAM_STREAM);
-			}
-			if (strcmp(optvalue, "raw") == 0){
-				socktype = SOCK_RAW;
-				buffer = malloc(BUFF_SIZE_RAW);
-			}
-			break;
-		case 's':
-			optvalue = optarg;
-			cset = atoi(optvalue);
-			break;
-		case 'c':
-			optvalue = optarg;
-			chan = atoi(optvalue);
+	while ((c = getopt(argc, argv, "rds:c:i:v")) != -1) {
+		switch (c) {
+		case 'r':
+			socktype = SOCK_RAW;
 			break;
 		case 'd':
-			optvalue = optarg;
-			dev_id = atoi(optvalue);
+			socktype = SOCK_DGRAM;
 			break;
-		case 'n':
-			optvalue = optarg;
-			strncpy(szio.devname, optvalue, ZIO_OBJ_NAME_LEN);
+		case 's':
+			zaddr.cset = atoi(optarg);
+			if (!strcmp(optarg, "all")) {
+				zaddr.cset = 0xffff;
+			}
 			break;
-		case 'b':
-			optvalue = optarg;
-			if (strcmp(optvalue, "cset") == 0)
-				chan = 0xFFFF;
-			if (strcmp(optvalue, "dev") == 0)
-				cset = 0xFFFF;
-			flag |= PF_ZIO_BIND;
+		case 'c':
+			zaddr.chan = atoi(optarg);
+			if (!strcmp(optarg, "all")) {
+				zaddr.cset = 0xffff;
+			}
+			break;
+		case 'i':
+			zaddr.dev_id = atoi(optarg);
 			break;
 		case 'v':
 			verbose++;
 			break;
 		}
 	}
+	if (optind != argc - 1)
+		print_help(argv[0], stderr);
+	strncpy(zaddr.devname, argv[optind], sizeof(zaddr.devname));
 
-	szio.sa_family = AF_ZIO;
-	szio.dev_id = dev_id;
-	szio.cset = cset;
-	szio.chan = chan;
-
-	sockfd = socket(AF_ZIO, socktype, 0);
-	if (sockfd < 0){
-		printf("Error creating socket: %s\n", strerror(errno));
-		return -1;
+	sock = socket(AF_ZIO, socktype, 0);
+	if (sock < 0){
+		fprintf(stderr, "%s: socket(PF_ZIO): %s\n",
+			argv[0], strerror(errno));
+		exit(1);
 	}
-
-	if(flag & PF_ZIO_BIND){
-		if (bind(sockfd, (struct sockaddr *)&szio, sizeof(szio)) < 0){
-			printf("Error binding socket: %s\n", strerror(errno));
-			return -2;
-		}
-	} else {
-		if (connect(sockfd, (struct sockaddr *)&szio, sizeof(szio))){
-			printf("Error connectin socket: %s\n", strerror(errno));
-			return -3;
-		}
+	/* FIXME: check what this PF_ZIO_BIND is */
+	if (connect(sock, (struct sockaddr *)&zaddr, slen)) {
+		fprintf(stderr, "%s: connect(PF_ZIO): %s\n",
+			argv[0], strerror(errno));
+		exit(1);
 	}
-
-	sziolen = sizeof(szfrom);
-
+	if (verbose) {
+		fprintf(stderr, "%s: connected to \"%.12s\","
+			" id 0x%04x, cset 0x%04x, chan 0x%04x\n", argv[0],
+			zaddr.devname, zaddr.dev_id, zaddr.cset, zaddr.chan);
+	}
 	while (1){
-		rxed = recvfrom(sockfd, buffer, sizeof(buffer), 0,
-				(struct sockaddr *)&szfrom, &sziolen);
-		if (verbose)
-			printf("Received %d bytes from device %s id %d channel %d cset %d\n",
-				rxed, szfrom.devname, szfrom.dev_id, szfrom.chan, szfrom.cset);
-		if (rxed < 0){
-			if (errno == EAGAIN){
-				printf("EAGAIN\n");
+		slen = sizeof(peer);
+		rxed = recvfrom(sock, buffer, sizeof(buffer), 0,
+				(struct sockaddr *)&peer, &slen);
+		if (rxed < 0) {
+			if (errno == EAGAIN)
 				continue;
-			}
-			printf("%s\n", strerror(errno));
-			break;
+			fprintf(stderr, "%s: recvfrom(): %s\n", argv[0],
+				strerror(errno));
+			exit(1);
 		}
-		dumpstruct(buffer, rxed);
-	}
 
-	close(sockfd);
+		if (verbose)
+			fprintf(stderr, "%s: received %i bytes from \"%.12s\","
+				" id %0x084x, cset 0x%04x, chan 0x%04x\n",
+				argv[0], rxed, peer.devname, peer.dev_id,
+				peer.cset, peer.chan);
+		fwrite(buffer, 1, rxed, stdout);
+		fflush(stdout);
+	}
+	close(sock);
 	return 0;
 }
