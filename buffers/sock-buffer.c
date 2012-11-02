84787315f5b6d5e63e3a8d3b94f24ee3fb885bfb
Alessandro Rubini <rubini@gnudd.com>
2012-11-03 00:43:37 +0100

    sock-buffer: cleanup
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>
    
    Conflicts:
    
    	buffers/sock-buffer.c

--- buffers/sock-buffer.c
+++ buffers/sock-buffer.c
@@ -10,7 +10,6 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
-#include <linux/sched.h>
 #include <linux/list.h>
 #include <linux/err.h>
 #include <linux/fs.h>
@@ -20,18 +19,13 @@
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 #include <linux/skbuff.h>
+#include <linux/net.h>
 #include <net/sock.h>
 #include <net/tcp_states.h> /* TCP_ESABLISHED is needed for SOCK_STREAM */
-#include <linux/netdevice.h>
-#include <linux/skbuff.h>
-#include <linux/net.h>
-#include <linux/device.h>
 
 #define __ZIO_INTERNAL__
 #include <linux/zio.h>
 #include <linux/zio-buffer.h>
-#include <linux/zio-trigger.h>
-#include <linux/zio-sysfs.h>
 #include <linux/zio-user.h>
 #include <linux/zio-sock.h>
 
@@ -51,54 +45,41 @@ struct zio_sysfs_operations zn_sysfs_ops = {
 
 static struct kmem_cache *zn_block_memcache;
 
-/*=====*/
-
-struct net_device *zn_get_output_device(struct zio_addr *zaddr)
-{
-	/*TODO Inline?*/
-	/*TODO Implement a working logic. Just for test, now i'll use my iface*/
-	return zn_netdev;
-}
-
-static struct net_device *get_input_device(void)
-{
-	/*TODO Inline?*/
-	/*TODO Implement a working logic. Just for test, now i'll use my iface*/
-	return zn_netdev;
-}
-
-/*Open socket list */
+/* Open socket list */
 struct list_head zn_sock_list;
 
-/* Alloc is called by the socket-layer sendmsg (for output) and by the trigger
- * (for input) */
+/* Called by the socket-layer sendmsg (output) and by the trigger (input) */
 static struct zio_block *zn_alloc_block(struct zio_bi *bi,
-					 struct zio_control *ctrl,
-					 size_t datalen, gfp_t gfp)
+					struct zio_control *ctrl,
+					size_t datalen, gfp_t gfp)
 {
 	struct zn_instance *zni = to_zni(bi);
 	struct zn_item *item;
 	struct sk_buff *skb;
 	struct zn_cb *cb;
 	void *ptr;
+	const int headspace = sizeof(struct ethhdr) + NET_ZIO_ALIGN
+		+ ZIO_CONTROL_SIZE;
 
 	item = kmem_cache_zalloc(zn_block_memcache, gfp);
 
-	/*Alloc skb to hold data going to/coming from the framework*/
-	skb = alloc_skb(sizeof(struct ethhdr)+ NET_ZIO_ALIGN + ZIO_CONTROL_SIZE + datalen, gfp);
+	/* Alloc skb to hold data going to/coming from the framework*/
+	skb = alloc_skb(headspace + datalen, gfp);
 	if (!skb || !item) {
 		printk("%s: problem allocating skb or item\n", __func__);
 		goto out;
 	}
 
-	skb_reserve(skb, sizeof(struct ethhdr) + ZIO_CONTROL_SIZE + NET_ZIO_ALIGN);
-	/*The trigger will store data without access to the sk_buff, so we
-	 * prepare it for him with skb_put*/
+	skb_reserve(skb, headspace);
+	/*
+	 * The trigger will store data without access to the sk_buff, so we
+	 * prepare it for him with skb_put
+	 */
 	item->block.data = skb_put(skb, datalen);
 
 	memcpy(skb_push(skb, ZIO_CONTROL_SIZE), ctrl, ZIO_CONTROL_SIZE);
 
-	/*Build ethernet header*/
+	/* Build ethernet header (not used by now */
 	dev_hard_header(skb, zn_netdev, ETH_P_ZIO, zn_netdev->dev_addr,
 			zn_netdev->dev_addr, skb->len);
 
@@ -124,8 +105,7 @@ out:
 	return ERR_PTR(-ENOMEM);
 }
 
-/* Free is called by socket-layer recvmsg (for input) or
- * by the trigger (for output) */
+/* Called by socket-layer recvmsg (input) or by the trigger (output) */
 static void zn_free_block(struct zio_bi *bi, struct zio_block *block)
 {
 	struct zn_item *item;
@@ -136,7 +116,7 @@ static void zn_free_block(struct zio_bi *bi, struct zio_block *block)
 	kmem_cache_free(zn_block_memcache, item);
 }
 
-/* Store is called by the trigger (for input) or by ndo_hard_xmit (for output)*/
+/* Called by the trigger (input) or by ndo_hard_xmit (output) */
 static int zn_store_block(struct zio_bi *bi, struct zio_block *block)
 {
 	struct zn_instance *zni = to_zni(bi);
@@ -149,15 +129,17 @@ static int zn_store_block(struct zio_bi *bi, struct zio_block *block)
 		return -EINVAL;
 	}
 
+	/* We take blocks and enqueue them to the network subsystem */
 	if ((bi->flags & ZIO_DIR) == ZIO_DIR_INPUT) {
-		/*We take blocks and enqueue them to the network subsystem*/
-		/*When the trigger tries to store_block, we don't save it in
-		 * the buffer but exploit the netif_rx queue to send block
-		 * to the user*/
+		/*
+		 * When the trigger tries to store_block, we don't
+		 * save it in the buffer but exploit the netif_rx
+		 * queue to send block to the user
+		 */
 		skb = item->skb;
 		skb->protocol = cpu_to_be16(ETH_P_ZIO);
 		skb->pkt_type = PACKET_HOST;
-		skb->dev = get_input_device();
+		skb->dev = zn_netdev;
 		ret = netif_rx_ni(skb);
 		return ret;
 	}
@@ -180,7 +162,7 @@ out_unlock:
 	return -ENOSPC;
 }
 
-/* Retr is called by the trigger (for output)*/
+/* Called by the trigger (output) */
 static struct zio_block *zn_retr_block(struct zio_bi *bi)
 {
 	struct zn_instance *zni;
@@ -202,7 +184,7 @@ static struct zio_block *zn_retr_block(struct zio_bi *bi)
 
 }
 
-/* Create is called by zio for each channel electing to use this buffer type */
+/* Called by zio for each channel electing to use this buffer type */
 static struct zio_bi *zn_create(struct zio_buffer_type *zbuf,
 				 struct zio_channel *chan)
 {
@@ -216,7 +198,7 @@ static struct zio_bi *zn_create(struct zio_buffer_type *zbuf,
 	return &zni->bi;
 }
 
-/* destroy is called by zio on channel removal or if it changes buffer type */
+/* Called by zio on channel removal or if it changes buffer type */
 static void zn_destroy(struct zio_bi *bi)
 {
 	struct zn_instance *zni = to_zni(bi);
@@ -246,7 +228,7 @@ static struct zio_buffer_type zn_buffer = {
 	},
 	.s_op = &zn_sysfs_ops,
 	.b_op = &zn_buffer_ops,
-	/*.f_op = &zio_generic_file_operations, TODO, should enable these*/
+	/*.f_op = &zio_generic_file_operations, TODO, should enable these? */
 };
 
 static int __init zn_init(void)
@@ -254,18 +236,13 @@ static int __init zn_init(void)
 	struct zn_priv *priv;
 	int ret;
 
-	/*Register new socket protocol*/
 	sock_register(&zn_protocol_family);
-
-	/*Register packet rcv handler*/
 	dev_add_pack(&zn_packet);
-
-	/*Creating network device*/
 	zn_netdev = alloc_etherdev(sizeof(struct zn_priv));
 	if (!zn_netdev) {
 		dev_remove_pack(&zn_packet);
 		sock_unregister(PF_ZIO);
-		return -ENODEV; /*FIXME*/
+		return -ENODEV; /* FIXME */
 	}
 	strcpy(zn_netdev->name, "zio");
 	zn_netdev->netdev_ops = &zn_netdev_ops;
@@ -275,7 +252,7 @@ static int __init zn_init(void)
 	priv = netdev_priv(zn_netdev);
 	register_netdev(zn_netdev);
 
-	/*Register buffer*/
+	/* Register memcache for the buffer */
 	zn_block_memcache = kmem_cache_create("zio-sock",
 					      sizeof(struct zn_item),
 					      __alignof__(struct zn_item),
@@ -290,24 +267,17 @@ static int __init zn_init(void)
 
 	ret = zio_register_buf(&zn_buffer, "socket");
 
-	/*Init socket list...*/
 	INIT_LIST_HEAD(&zn_sock_list);
 	return ret;
 }
 
 static void __exit zn_exit(void)
 {
-	/*Remove net device*/
+	/* FIXME: the order is wrong */
 	unregister_netdev(zn_netdev);
 	free_netdev(zn_netdev);
-
-	/*Remove packet rcv handler*/
 	dev_remove_pack(&zn_packet);
-
-	/*Unregister socket-layer*/
 	sock_unregister(PF_ZIO);
-
-	/*Unregister zio buffer*/
 	zio_unregister_buf(&zn_buffer);
 }
 
